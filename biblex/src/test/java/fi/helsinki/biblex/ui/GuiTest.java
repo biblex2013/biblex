29b3e5d80f78e00e98b0d884ae2587ff6c3d46be
danefl <jtmikkon@gmail.com>
2013-04-12 17:50:37 +0300

    fest test works

--- biblex/src/test/java/fi/helsinki/biblex/ui/GuiTest.java
+++ biblex/src/test/java/fi/helsinki/biblex/ui/GuiTest.java
@@ -1,6 +1,9 @@
 package fi.helsinki.biblex.ui;
 
 import fi.helsinki.biblex.App;
+import fi.helsinki.biblex.exporter.Exporter;
+import fi.helsinki.biblex.storage.Storage;
+import javax.swing.JFrame;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,20 +15,30 @@ import org.fest.util.*;
 import org.fest.assertions.*;
 import org.fest.swing.annotation.GUITest;
 import org.fest.swing.*;
+import org.fest.swing.fixture.FrameFixture;
 
 /**
  *
  * @author jtmikkon
  */
-public class GuiTest extends TestCase {
+public class GuiTest {
     
     private GUI gui;
-    private EntryPane entryPane;
-    private MainWindow mainWindow;
-    private ReferenceWindow refWindow;
+    private JFrame entryPane;
+    private JFrame mainWindow;
+    private JFrame refWindow;
     private App app;
+    private Storage storage;
+    private Exporter exporter;
+    private FrameFixture testFrame;
     
-    public GuiTest() {
+    public GuiTest() throws InstantiationException, IllegalAccessException {
+        app.createInstance();
+        app = App.getInstance();
+        gui = app.getP_gui();
+        refWindow = gui.getWindow();
+        
+        
         
     }
     
@@ -47,7 +60,15 @@ public class GuiTest extends TestCase {
     }
     
     @Test
-    public void testTest() {
-        assertTrue(true);
+    public void refWindowTest() {
+        FrameFixture testFrame = new FrameFixture(refWindow);
+        testFrame.show();
+        
+        testFrame.comboBox().selectItem("inproceedings");
+        testFrame.comboBox().requireSelection("inproceedings");
+        
+        testFrame.textBox().enterText("lols");
+        
+        
     }
 }
