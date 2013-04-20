15bd7cae152feff690e3b8b666eefcb585d834a4
Henry Carlson <henry.carlson@gmail.com>
2013-04-20 11:06:59 +0300

    FileChoosereihin lisätty BibTeX-filtteri

--- biblex/src/main/java/fi/helsinki/biblex/ui/ExportFileChooser.java
+++ biblex/src/main/java/fi/helsinki/biblex/ui/ExportFileChooser.java
@@ -1,17 +1,25 @@
 package fi.helsinki.biblex.ui;
 
-import java.util.MissingResourceException;
 import javax.swing.*;
 
 public class ExportFileChooser extends GenericFileChooser {
     public ExportFileChooser() {
         this.setFileSelectionMode(JFileChooser.FILES_ONLY);
 				this.setApproveButtonMnemonic('e');
+        this.setFileFilter(new BibTexFile());
     }
 
     public String getFileName() {
         if (this.showDialog(null, "Export") != JFileChooser.APPROVE_OPTION)
             return null;
-        return this.getSelectedFile().getAbsolutePath();
+
+        String path = this.getSelectedFile().getAbsolutePath();
+
+        // set extension if necessary
+        if (BibTexFile.class.isInstance(this.getFileFilter()) && path.lastIndexOf(".") == -1) {
+            path += ".bib";
+        }
+
+        return path;
     }
 }
